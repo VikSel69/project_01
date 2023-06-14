47b1a8960b9e9ef540e9a6b6071a4584fdc6fca9
viktor_seleznev <v.e.seleznev@mail.ru>
2023-06-14 13:56:40 +0800

    Добавлено решение задачи task 1_2.

---
+++ lvl_1/task_1.2.py
@@ -0,0 +1,80 @@
+import random
+import datetime
+
+# Задача 1.2.
+
+# Пункт A.
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+# Решение задачи 1.2 пункт А
+
+sum_time = datetime.timedelta()
+i = 0
+
+while i < 3:
+    sound_time = random.choice(my_favorite_songs)[1]
+    time_to_min = int(sound_time)
+    time_to_sec = round((sound_time - time_to_min) * 100)
+    sum_time += datetime.timedelta(minutes=time_to_min, seconds=time_to_sec)
+    i += 1
+
+print(f'Три песни звучат {sum_time} минут')
+
+# Пункт B.
+# Есть словарь песен
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+# Решение задачи 1.2 пункт В
+
+sum_time = datetime.timedelta()
+key_lst = []
+i = 0
+
+for key in my_favorite_songs_dict:
+    key_lst.append(key)
+
+while i < 3:
+    sound_time = my_favorite_songs_dict[random.choice(key_lst)]
+    time_to_min = int(sound_time)
+    time_to_sec = round((sound_time - time_to_min) * 100)
+    sum_time += datetime.timedelta(minutes=time_to_min, seconds=time_to_sec)
+    i += 1
+
+print(f'Три песни звучат {sum_time} минут')
+
+# Дополнительно для пунктов A и B
+# Пункт C.
+# Сгенерируйте случайные песни с помощью модуля random
+# import random
+
+# Дополнительно
+# Пункт D.
+# Переведите минуты и секунды в формат времени. Используйте модуль datetime
